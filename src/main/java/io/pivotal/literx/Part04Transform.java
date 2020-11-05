c59014f62a5a948f2180d545c66a0e761c9693d3
razvan.gaston <razvan.gaston@softvision.com>
2020-11-05 15:46:56 +0200

    [webflux | reactor] Transform a Publisher's Events into another Objects/events

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -4,6 +4,8 @@ import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 /**
  * Learn how to transform values.
  *
@@ -11,25 +13,45 @@ import reactor.core.publisher.Mono;
  */
 public class Part04Transform {
 
+
+	private Function<User, User> capitalizeUserName = (user) -> new User(
+			user.getUsername().toUpperCase(),
+			user.getFirstname().toUpperCase(),
+			user.getLastname().toUpperCase()
+	);
+
+
+	private User nameToUpperCase(User input){
+		return new User(
+				input.getUsername().toUpperCase(),
+				input.getFirstname().toUpperCase(),
+				input.getLastname().toUpperCase()
+		);
+	}
+
+
+
 //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
+	// TODOx Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(capitalizeUserName);
 	}
+
+
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName
+	// TODOx Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(this::nameToUpperCase);
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	// TODOx Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
