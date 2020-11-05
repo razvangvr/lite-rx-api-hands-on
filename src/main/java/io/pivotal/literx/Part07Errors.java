83f425bd1e8205eec2124a55d147dc879f495b79
razvan.gaston <razvan.gaston@softvision.com>
2020-11-05 16:31:37 +0200

    [webflux | reactor] handle exceptions

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -21,6 +21,8 @@ import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Function;
+
 /**
  * Learn how to deal with errors.
  *
@@ -31,35 +33,51 @@ public class Part07Errors {
 
 //========================================================================================
 
-	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
-	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
-	}
+    // TODOx Return a Mono<User> containing User.SAUL when an error occurs in the input Mono,
+    //  else do not change the input Mono.
+    Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
+        return mono.onErrorReturn(User.SAUL);
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
-	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
-	}
+    // TODOx Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux,
+    //  else do not change the input Flux.
+    Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
+        return flux.onErrorResume(
+                (e) -> {
+                    e.printStackTrace();
+                    return Flux.just(User.SAUL, User.JESSE);
+                }
+        );
+    }
 
 //========================================================================================
 
-	// TODO Implement a method that capitalizes each user of the incoming flux using the
-	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    // TODOx Implement a method that capitalizes each user of the incoming flux using the
+    // #capitalizeUser method and emits an error containing a GetOutOfHereException error
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        return flux.map(user -> {
+            try {
+                return this.capitalizeUser(user);
+            } catch (GetOutOfHereException e) {
+                throw new RuntimeException(e);
+            }
+        }).onErrorResume(throwable -> {
+            Throwable ex = throwable.getCause();
+            return Flux.error(ex);
+        });
+    }
 
-	User capitalizeUser(User user) throws GetOutOfHereException {
-		if (user.equals(User.SAUL)) {
-			throw new GetOutOfHereException();
-		}
-		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
-	}
+    User capitalizeUser(User user) throws GetOutOfHereException {
+        if (user.equals(User.SAUL)) {
+            throw new GetOutOfHereException();
+        }
+        return new User(user.getUsername(), user.getFirstname(), user.getLastname());
+    }
 
-	protected final class GetOutOfHereException extends Exception {
-	    private static final long serialVersionUID = 0L;
-	}
+    protected final class GetOutOfHereException extends Exception {
+        private static final long serialVersionUID = 0L;
+    }
 
 }
